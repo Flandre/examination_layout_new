cfc26f519285ee5230047b22d6654c78d2819207
Flandre <flantan233@gmail.com>
2018-10-24 16:50:16 +0800

    update

---
+++ PC/index.js
@@ -0,0 +1,101 @@
+var getData = {
+  "status_code": "ok",
+  "message": {
+    "lLeg": [
+      {
+        "name": "合格",
+        "value": "1"
+      },
+      {
+        "name": "不合格",
+        "value": "0"
+      }
+    ],
+    "lArm": [
+      {
+        "name": "合格",
+        "value": "1"
+      },
+      {
+        "name": "不合格",
+        "value": "0"
+      },
+      {
+        "name": "手指末节残缺",
+        "value": "2"
+      },
+      {
+        "name": "左手三指健全,双手手掌完整",
+        "value": "4"
+      }
+    ],
+    "rCorrect": [
+      {
+        "name": "是",
+        "value": "1"
+      },
+      {
+        "name": "否",
+        "value": "0"
+      }
+    ],
+    "lCorrect": [
+      {
+        "name": "是",
+        "value": "1"
+      },
+      {
+        "name": "否",
+        "value": "0"
+      }
+    ],
+    "rLeg": [
+      {
+        "name": "合格",
+        "value": "1"
+      },
+      {
+        "name": "不合格",
+        "value": "0"
+      }
+    ],
+    "aid": [
+      {
+        "name": "有",
+        "value": "1"
+      },
+      {
+        "name": "无",
+        "value": "0"
+      }
+    ],
+    "obstacle": [
+      {
+        "name": "有",
+        "value": "0"
+      },
+      {
+        "name": "无",
+        "value": "1"
+      }
+    ],
+    "rArm": [
+      {
+        "name": "合格",
+        "value": "1"
+      },
+      {
+        "name": "不合格",
+        "value": "0"
+      },
+      {
+        "name": "手指末节残缺",
+        "value": "2"
+      },
+      {
+        "name": "右手拇指缺失",
+        "value": "3"
+      }
+    ]
+  }
+}
